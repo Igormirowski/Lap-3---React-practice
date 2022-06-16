4ad241ba791de1f9a4e1f4472eedda8e23864f91
“Igormirowski” <igorpmirowski@gmail.com>
2022-06-16 12:26:53 +0100

    update friend to typed name

--- newsiesigor/src/components/Greeting.js
+++ newsiesigor/src/components/Greeting.js
@@ -3,12 +3,32 @@ import React, { useState } from 'react';
 
 
 const Greeting = () => {
+    const [username, setUsername] = useState('friend')
+    const [nameInput, setNameInput] = useState('') // control my input
+
+
+    const handleInput = (e) => {
+        setNameInput(e.target.value)
+    }
+    
+    const handleFormSubmit = (e)=> {
+    e.preventDefault()
+    setUsername(nameInput)
+}
+
     return (
         <>
-        <h3 aria-label="greeting">Hi there, username/friend!</h3>
-        <form>
+        <h3 aria-label="greeting">Hi there,{username}!</h3>
+
+        <form onSubmit={handleFormSubmit}>
             <label htmlFor="username">Username</label>
-            <input type="text" id="username" placeholder="That's not my name"/>
+            <input 
+            type="text"
+            id="username" 
+            placeholder="That's not my name" 
+            value={nameInput}
+            onChange={handleInput}
+            />
             <input type="submit" value="Update!"></input>
         </form>
         </>
