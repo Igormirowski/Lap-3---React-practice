811c4adb038dc5dfb2873136702b35950ab16690
“Igormirowski” <igorpmirowski@gmail.com>
2022-06-16 12:07:18 +0100

    add heart icon and make it functional

--- newsiesigor/src/components/FaveButton.js
+++ newsiesigor/src/components/FaveButton.js
@@ -1,18 +1,25 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 
 
 const FaveButton = () => {
+    const [faved, setFaved] = useState(false)
+    const handleFaveBtnClick = () => {
+        setFaved(!faved)
+    }
     return (
-        <>
-        <span>
-        style={{ color: 'red'}}
-        onClick = change color when
-            ❤️
+        
+        <span
+        style={{ color: faved ? 'red' : 'grey'}}
+        onClick={handleFaveBtnClick}
+        >
+            ♥
+            {/* ★ */}
         </span>
-        </> 
+     
     )
 }
 
 
 export default FaveButton;
+
